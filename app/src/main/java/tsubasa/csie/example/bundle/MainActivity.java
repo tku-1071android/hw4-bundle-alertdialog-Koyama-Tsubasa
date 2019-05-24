5ad87a91a75c2a7af12cad69801af4e5add55d1e
Koyama-Tsubasa <a0918050152@gmail.com>
2019-05-24 12:03:26 +0800

    shit

--- app/src/main/java/tsubasa/csie/example/bundle/MainActivity.java
+++ app/src/main/java/tsubasa/csie/example/bundle/MainActivity.java
@@ -1,13 +1,55 @@
 package tsubasa.csie.example.bundle;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private EditText etProgramming,etDataStructure,etAlgorithms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
     }
+
+    private void findViews() {
+        etProgramming = (EditText)findViewById(R.id.etProgramming);
+        etDataStructure = (EditText)findViewById(R.id.etDataStructure);
+        etAlgorithms = (EditText)findViewById(R.id.etAlgorithms);
+    }
+
+    private boolean isValid (EditText edittext) {
+        String pattern = "1[0]{2} | [0-9]{1,2}";
+        String text = edittext.getText().toString();
+        if (!text.matches(pattern)) {
+            edittext.setError("0~100");
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public void onSubmitClick (View view) {
+        boolean isValid = isValid(etProgramming) & isValid(etAlgorithms) & isValid(etDataStructure);
+        if (!isValid) {
+            return;
+        }
+        int programming = Integer.parseInt(etProgramming.getText().toString());
+        int datastructure = Integer.parseInt(etDataStructure.getText().toString());
+        int algorithms = Integer.parseInt(etAlgorithms.getText().toString());
+        Intent intent = new Intent(this,ResultActivity.class);
+        Bundle bundle  = new Bundle();
+        bundle.putInt("programming",programming);
+        bundle.putInt("datastructure",datastructure);
+        bundle.putInt("algorithms",algorithms);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/tsubasa/csie/example/bundle/ResultActivity.java
@@ -0,0 +1,39 @@
+package tsubasa.csie.example.bundle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.text.NumberFormat;
+
+public class ResultActivity extends Activity {
+
+    private TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstantstate) {
+        super.onCreate(savedInstantstate);
+        setContentView(R.layout.resultlayout);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        showResults();
+    }
+
+    private void showResults () {
+        NumberFormat nf = NumberFormat.getInstance();
+        Bundle bundle = getIntent().getExtras();
+        int programming = bundle.getInt("programming");
+        int datastructure = bundle.getInt("datastructure");
+        int algorithms = bundle.getInt("algorithms");
+        int sum = programming + datastructure + algorithms;
+        double average = sum/3.0;
+        String text = "programming = " + programming +
+                "\ndatastructure = " + datastructure +
+                "\nalgorithms = " + algorithms +
+                "\nsum = " + sum +
+                "\naverage = " + nf.format(average);
+        tvResult.setText(text);
+    }
+
+    public void onBackClick (View view) {finish();}
+
+}
